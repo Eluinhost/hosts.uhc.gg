cb736d579318860c611e8cd9e421c7607d3595e9
Eluinhost <graham_howden1@yahoo.co.uk>
2017-07-08 13:16:55 +0100

    add authentication required routes
    Signed-off-by: Eluinhost <graham_howden1@yahoo.co.uk>

--- frontend/app/components/App.tsx
+++ frontend/app/components/App.tsx
@@ -3,8 +3,11 @@ import { Username } from './Username';
 import { HostForm } from './HostForm';
 import { NonIdealState } from '@blueprintjs/core';
 import { BrowserRouter } from 'react-router-dom';
-import { Route, RouteComponentProps, Switch } from 'react-router';
+import { Route, RouteComponentProps, RouteProps, Switch } from 'react-router';
 import { LoginPage } from './LoginPage';
+import { omit } from 'ramda';
+import { connect } from 'react-redux';
+import { ApplicationState } from '../state/ApplicationState';
 
 const NavBar: React.SFC = () => (
   <nav className="pt-navbar .modifier">
@@ -25,16 +28,49 @@ const NotFoundPage: React.SFC<RouteComponentProps<any>> = () => (
   />
 );
 
+type AuthedRouteProps = {
+  permissions: string[];
+  required: string;
+} & RouteProps;
+
+const AuthedRoute: React.SFC<AuthedRouteProps> = (props) => {
+  if (!props.permissions.some(perm => perm === props.required))
+    return (
+      <NonIdealState
+        title="You do not have permission to do this"
+        visual="warning"
+      />
+    );
+
+  return <Route {...omit(['permissions', 'required'], props) as RouteProps} />;
+};
+
+type RoutesStateProps = {
+  permissions: string[];
+};
+
+const RoutesComponent : React.SFC<RoutesStateProps> = ({ permissions }) => (
+  <Switch>
+    <AuthedRoute path="/host" component={HostFormPage} required="host" permissions={permissions} />
+    <Route path="/login/:token" component={LoginPage} />
+    <Route component={NotFoundPage} />
+  </Switch>
+);
+
+function mapStateToProps(state: ApplicationState): RoutesStateProps {
+  return {
+    permissions: state.authentication.loggedIn ? state.authentication.data!.claims.permissions : [],
+  };
+}
+
+const Routes = connect<RoutesStateProps, {}, {}>(mapStateToProps)(RoutesComponent);
+
 export const App: React.SFC = () => (
   <BrowserRouter>
     <div className="pt-dark">
       <NavBar />
       <div className="app-container">
-        <Switch>
-          <Route path="/host" component={HostFormPage} />
-          <Route path="/login/:token" component={LoginPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
+        <Routes />
       </div>
     </div>
   </BrowserRouter>
