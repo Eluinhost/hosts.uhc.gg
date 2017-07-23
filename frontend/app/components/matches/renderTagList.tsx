d13a14324d50889a9076c4d4f51ede71f3a2669f
Eluinhost <graham_howden1@yahoo.co.uk>
2017-07-23 14:01:14 +0100

    fix tag colours

--- frontend/app/components/matches/renderTagList.tsx
+++ frontend/app/components/matches/renderTagList.tsx
@@ -2,5 +2,5 @@ import { Intent } from '@blueprintjs/core';
 import * as React from 'react';
 
 export const renderTagList = (intent: Intent, title: string, items: string[]) => items.map((item, index) =>
-  <span className={`pt-tag pt-intent-${Intent[intent]} pt-large`} title={title}>{item}</span>,
+  <span className={`pt-tag pt-intent-${Intent[intent].toLowerCase()} pt-large`} title={title}>{item}</span>,
 );
