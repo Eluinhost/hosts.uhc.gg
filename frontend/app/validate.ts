783d0cb825a40131e608d743991ee61a06b90389
Eluinhost <graham_howden1@yahoo.co.uk>
2017-07-30 00:09:25 +0100

    fix(?) validation
    Signed-off-by: Eluinhost <graham_howden1@yahoo.co.uk>

--- frontend/app/validate.ts
+++ frontend/app/validate.ts
@@ -2,10 +2,14 @@ import { DataShape, FormErrors } from 'redux-form';
 import { mapObjIndexed } from 'ramda';
 import { ReactElement } from 'react';
 
+export type ValidationResult = ReactElement<any> | string | { _error?: string } | undefined;
+
 export type Spec<T> = {
-  [P in keyof T]: (prop: T[P], obj: T) => ReactElement<any> | string | { _error?: string } | undefined;
+  [P in keyof T]: (prop: T[P], obj: T) => ValidationResult;
 };
 
 export const validate = <T extends DataShape>(spec: Spec<T>) => <V extends T>(obj: V): FormErrors<T> =>
-  mapObjIndexed((v, k, o) => spec[k](v, o))(obj) as any as FormErrors<T>;
+  mapObjIndexed(
+    (specFn: any, k: string, o: T) => specFn(obj[k], obj),
+  )(spec) as any as FormErrors<T>;
 
