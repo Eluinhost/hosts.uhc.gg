9a3d38ae1941a70891fa0a076f59557bff5fea43
Eluinhost <graham_howden1@yahoo.co.uk>
2017-07-31 01:53:54 +0100

    make frontend send times as exact minutes

--- frontend/app/components/host/nextAvailableSlot.ts
+++ frontend/app/components/host/nextAvailableSlot.ts
@@ -1,7 +1,11 @@
 import * as moment from 'moment';
 
 export const nextAvailableSlot = (): moment.Moment => {
-  const in30 = moment.utc().add(30, 'minute');
+  // set seconds + millis to zero and add 31 minutes to find the next whole minute in 30 minutes time
+  const in30 = moment.utc()
+    .seconds(0)
+    .milliseconds(0)
+    .add(31, 'minute');
 
   const targetMinute = in30.get('minute');
   const diffToNext15 = (Math.ceil(targetMinute / 15) * 15) - targetMinute;
