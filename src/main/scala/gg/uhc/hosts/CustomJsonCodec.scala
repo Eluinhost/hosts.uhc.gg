3b3a4b773f780cd21ca75cff22a4971ef62b934f
Eluinhost <graham_howden1@yahoo.co.uk>
2017-08-13 19:14:14 +0100

    just add roles to the output json instead of making a new type

--- src/main/scala/gg/uhc/hosts/CustomJsonCodec.scala
+++ src/main/scala/gg/uhc/hosts/CustomJsonCodec.scala
@@ -1,9 +1,17 @@
 package gg.uhc.hosts
 
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+import gg.uhc.hosts.database.MatchRow
+import io.circe.JsonObject
+import io.circe.syntax._
 import io.circe.generic.AutoDerivation
 import io.circe.java8.time.TimeInstances
 
 trait CustomJsonCodec extends FailFastCirceSupport with AutoDerivation with TimeInstances
 
-object CustomJsonCodec extends CustomJsonCodec
+object CustomJsonCodec extends CustomJsonCodec {
+  implicit class MatchRowExtensions(m: MatchRow) {
+    def toJsonWithRoles(roles: List[String]): JsonObject =
+      m.asJsonObject.add("roles", roles.asJson)
+  }
+}
--- src/main/scala/gg/uhc/hosts/endpoints/matches/ShowMatch.scala
+++ src/main/scala/gg/uhc/hosts/endpoints/matches/ShowMatch.scala
@@ -6,8 +6,8 @@ import akka.http.scaladsl.server.Route
 import doobie.imports._
 import gg.uhc.hosts.CustomJsonCodec
 import gg.uhc.hosts.database.{Database, MatchRow}
-import gg.uhc.hosts.endpoints.matches.ListMatches.MatchRowResponse
 import gg.uhc.hosts.endpoints.{CustomDirectives, EndpointRejectionHandler}
+import io.circe.JsonObject
 
 import scalaz.OptionT
 
@@ -15,7 +15,7 @@ class ShowMatch(directives: CustomDirectives, database: Database) {
   import CustomJsonCodec._
   import directives._
 
-  def fetchData(id: Long): ConnectionIO[Option[MatchRowResponse]] =
+  def fetchData(id: Long): ConnectionIO[Option[JsonObject]] =
     (for {
       row ← OptionT[ConnectionIO, MatchRow] {
         database.matchById(id)
@@ -23,36 +23,7 @@ class ShowMatch(directives: CustomDirectives, database: Database) {
       perms ← OptionT[ConnectionIO, List[String]] {
         database.getPermissions(row.author).map(Some(_))
       }
-    } yield
-      MatchRowResponse(
-        id = row.id,
-        author = row.author,
-        opens = row.opens,
-        address = row.address,
-        ip = row.ip,
-        scenarios = row.scenarios,
-        tags = row.tags,
-        teams = row.teams,
-        size = row.size,
-        customStyle = row.customStyle,
-        count = row.count,
-        content = row.content,
-        region = row.region,
-        removed = row.removed,
-        removedBy = row.removedBy,
-        removedReason = row.removedReason,
-        created = row.created,
-        location = row.location,
-        version = row.version,
-        slots = row.slots,
-        length = row.length,
-        mapSize = row.mapSize,
-        pvpEnabledAt = row.pvpEnabledAt,
-        approvedBy = row.approvedBy,
-        roles = perms,
-        hostingName = row.hostingName,
-        tournament = row.tournament
-      )).run
+    } yield row.toJsonWithRoles(perms)).run
 
   def apply(id: Long): Route =
     handleRejections(EndpointRejectionHandler()) {
