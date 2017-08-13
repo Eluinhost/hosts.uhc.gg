bc919288679d3a52964cf18071189ab40054e3b3
Eluinhost <graham_howden1@yahoo.co.uk>
2017-08-13 17:11:49 +0100

    add switch field type

---
+++ frontend/app/components/fields/SwitchField.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { BaseFieldProps, Field, WrappedFieldProps } from 'redux-form';
+import { FieldWrapper } from './FieldWrapper';
+import { Switch } from '@blueprintjs/core';
+
+export interface SwitchFieldProps extends BaseFieldProps {
+  readonly label: string;
+  readonly disabled?: boolean;
+  readonly className?: string;
+}
+
+class Switcher extends React.Component<WrappedFieldProps<any> & SwitchFieldProps> {
+  onChange = (): void => {
+    if (this.props.disabled)
+      return;
+
+    this.props.input!.onChange(!this.props.input!.value, undefined, undefined);
+    this.props.input!.onBlur(!this.props.input!.value, undefined, undefined);
+  }
+
+  render() {
+    return (
+      <FieldWrapper meta={this.props.meta} required>
+        <Switch
+          checked={!!this.props.input!.value}
+          className={this.props.className || ''}
+          disabled={this.props.disabled}
+          label={this.props.label}
+          onChange={this.onChange}
+        />
+      </FieldWrapper>
+    );
+  }
+}
+
+export const SwitchField: React.SFC<SwitchFieldProps> = props => <Field {...props} component={Switcher} />;
