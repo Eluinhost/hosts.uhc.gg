d69aede223e0a82c01d271dc11036ea13dce2166
Eluinhost <graham_howden1@yahoo.co.uk>
2017-08-16 15:27:51 +0100

    processing of old data too

--- ubl/README.md
+++ ubl/README.md
@@ -1,3 +1,9 @@
-Latest data as JSON here: https://spreadsheets.google.com/feeds/list/1VdyBZs4B-qoA8-IijPvbRUVBLfOuU9I5fV_PhuOWJao/od6/public/values?alt=json
+Latest data as XML here: 
 
-Running `node .` will generate `output.sql` containing some insert queries for the data
+current: `https://spreadsheets.google.com/feeds/list/1VdyBZs4B-qoA8-IijPvbRUVBLfOuU9I5fV_PhuOWJao/od6/private/values`
+
+old: `https://spreadsheets.google.com/feeds/list/1VdyBZs4B-qoA8-IijPvbRUVBLfOuU9I5fV_PhuOWJao/od5/private/values`
+
+Files should be placed in `/data` as json (https://codebeautify.org/xmlviewer, load url won't work have to c/p): `current.json` and `old.json` respectively
+
+Running `node .` will generate `output.sql` containing some (a lot) insert queries for the data
--- ubl/generateInsertStatements.js
+++ ubl/generateInsertStatements.js
@@ -1,64 +1,86 @@
-const { join, pipe, map } = require('ramda');
+const {join, pipe, map, concat, filter} = require('ramda');
 const moment = require('moment');
 const fs = require('fs');
 
-const rawData = require('./data.json');
+const old = require('./data/old');
+const current = require('./data/current');
 
 const parseDate = dateString => {
-    const parsed = moment.utc(
-        dateString
-            .replace('Apil', 'April')
-            .replace('Fabruary', 'February')
-            .replace('September 31', 'October 1')
-            .replace(' ', ''), // fix spacing issues by removing them all
-        'MMMMD,YYYY'
-    );
+  const parsed = moment.utc(
+    dateString
+      .replace('Apil', 'April')
+      .replace('Fabruary', 'February')
+      .replace('September 31', 'October 1')
+      .replace('June 31', 'July 1')
+      .replace('November 31', 'December 1')
+      .replace(' ', ''), // fix spacing issues by removing them all
+    'MMMMD,YYYY'
+  );
 
-    if (!parsed.isValid())
-        throw new Error(`invalid date: ${dateString}`);
+  if (!parsed.isValid())
+    throw new Error(`invalid date: ${dateString}`);
 
-    return parsed.toISOString();
+  return parsed.toISOString();
 };
 
 const uuidRegex = /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/
 
 const parseUuid = uuid => {
-    let toParse = uuid.trim();
-    switch (toParse.length) {
-        case 32:
-            toParse =
-                toParse.substring(0, 8) + '-' +
-                toParse.substring(8, 12) + '-' +
-                toParse.substring(12, 16) + '-' +
-                toParse.substring(16, 20) + '-' +
-                toParse.substring(20, 32);
-            break;
-        case 36:
-            break;
-        default:
-            throw new Error(`invalid uuid length (${uuid.length}): ${uuid}`);
-    }
+  let toParse = uuid.trim();
 
-    if (!uuidRegex.test(toParse))
-        throw new Error(`invalid uuid: ${uuid}`);
+  if (toParse === 'Couldnt do...?')
+    return '00000000-0000-0000-0000-000000000000';
 
-    return toParse;
+  switch (toParse.length) {
+    case 32:
+      toParse =
+        toParse.substring(0, 8) + '-' +
+        toParse.substring(8, 12) + '-' +
+        toParse.substring(12, 16) + '-' +
+        toParse.substring(16, 20) + '-' +
+        toParse.substring(20, 32);
+      break;
+    case 36:
+      break;
+    default:
+      throw new Error(`invalid uuid length (${uuid.length}): ${uuid}`);
+  }
+
+  if (!uuidRegex.test(toParse))
+    throw new Error(`invalid uuid: ${uuid}`);
+
+  return toParse;
 };
 
-const parseItem = item => {
-    try {
-        return {
-            created: parseDate(item.gsx$datebanned.$t),
-            expires: parseDate(item.gsx$expirydate.$t),
-            ign: item.gsx$ign.$t,
-            uuid: parseUuid(item.gsx$uuid.$t),
-            reason: item.gsx$reason.$t,
-            link: item.gsx$case.$t
-        };
-    } catch (err) {
-        throw new Error(`Failed to parse record ${JSON.stringify(item)} : ${err}`);
-    }
+const parseCurrentItem = item => {
+  try {
+    return {
+      created: parseDate(item.datebanned.__text),
+      expires: parseDate(item.expirydate.__text),
+      ign: item.ign.__text,
+      uuid: parseUuid(item.uuid.__text),
+      reason: item.reason.__text,
+      link: item.case.__text
+    };
+  } catch (err) {
+    throw new Error(`Failed to parse record ${JSON.stringify(item)} : ${err}`);
+  }
 };
+
+const parseOldItem = item => {
+  try {
+    return {
+      created: parseDate(item.bandate.__text),
+      expires: parseDate(item.unbandate.__text),
+      ign: item.player.__text,
+      uuid: parseUuid(item.uuid.__text),
+      reason: item.reason.__text,
+      link: item._ckd7g ? item._ckd7g.__text : 'Unknown'
+    };
+  } catch (err) {
+    throw new Error(`Failed to parse record ${JSON.stringify(item)} : ${err}`);
+  }
+}
 
 const generateStatement = item => `INSERT INTO ubl (ign, uuid, reason, created, expires, link) VALUES (
     '${item.ign.replace(/'/g, '\'\'')}',
@@ -69,12 +91,32 @@ const generateStatement = item => `INSERT INTO ubl (ign, uuid, reason, created, 
     '${item.link.replace(/'/g, '\'\'')}'
 )`;
 
-const convertData = pipe(
-    map(parseItem),
-    map(generateStatement),
-    statements => ['BEGIN TRANSACTION', ...statements, 'COMMIT TRANSACTION'],
-    join(';\n'),
-    data => fs.writeFileSync('output.sql', data, { encoding: 'utf-8' })
+const createOutputFile = pipe(
+  map(generateStatement),
+  statements => [
+    'BEGIN TRANSACTION',
+    'TRUNCATE TABLE ubl',
+    ...statements,
+    'COMMIT TRANSACTION'
+  ],
+  join(';\n'),
+  data => fs.writeFileSync('output.sql', data, {encoding: 'utf-8'})
 );
 
-convertData(rawData.feed.entry);
+const parsedData = concat(
+  map(parseCurrentItem)(current.feed.entry),
+  map(parseOldItem)(old.feed.entry)
+);
+
+const withoutInvalidUuids = filter(
+  item => {
+    if (item.uuid === '00000000-0000-0000-0000-000000000000') {
+      console.log(`Skipping entry due to invalid UUID: ${JSON.stringify(item)}`)
+      return false;
+    }
+    return true;
+  },
+  parsedData
+);
+
+createOutputFile(withoutInvalidUuids);
