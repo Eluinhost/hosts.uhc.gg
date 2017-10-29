3dfd5a70f5bf0427c1d581c3f2b4fadd9569cfb9
Eluinhost <graham_howden1@yahoo.co.uk>
2017-10-29 13:05:47 +0000

    1.0.0 release

--- project/Settings.scala
+++ project/Settings.scala
@@ -3,7 +3,7 @@ import sbt._
 object Settings {
   val organisation = "gg.uhc"
   val name         = "hosts"
-  val version      = "0.0.1"
+  val version      = "1.0.0"
 
   val scalacOptions = Seq(
     "-Xlint",
