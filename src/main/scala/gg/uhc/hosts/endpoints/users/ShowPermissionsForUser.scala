fee9d332b99e4352b8bd79eef9e476bc132c7995
Graham Howden <graham_howden1@yahoo.co.uk>
2020-03-30 20:37:46 +0100

    duplicate code

--- src/main/scala/gg/uhc/hosts/endpoints/users/ShowPermissionsForUser.scala
+++ src/main/scala/gg/uhc/hosts/endpoints/users/ShowPermissionsForUser.scala
@@ -2,7 +2,6 @@ package gg.uhc.hosts.endpoints.users
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
-import doobie._
 
 import gg.uhc.hosts.CustomJsonCodec
 import gg.uhc.hosts.database.Database
@@ -11,9 +10,6 @@ import gg.uhc.hosts.endpoints.{CustomDirectives, EndpointRejectionHandler}
 class ShowPermissionsForUser(database: Database, customDirectives: CustomDirectives) {
   import CustomJsonCodec._
   import customDirectives._
-
-  def listPermissionsForUser(username: String): ConnectionIO[List[String]] =
-    database.getPermissions(username)
 
   def apply(username: String): Route =
     handleRejections(EndpointRejectionHandler()) {
