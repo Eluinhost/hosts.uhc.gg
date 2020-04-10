70649cf1b33d9a1b8b6d13fd70cb04004d80e76e
Graham Howden <graham_howden1@yahoo.co.uk>
2020-04-11 00:28:19 +0100

    1.2.3 release

--- project/Settings.scala
+++ project/Settings.scala
@@ -3,7 +3,7 @@ import sbt._
 object Settings {
   val organisation = "gg.uhc"
   val name         = "hosts"
-  val version      = "1.2.2"
+  val version      = "1.2.3"
 
   val scalacOptions = Seq(
     "-Xlint",
