076fe3c74fb45adf820e38ae51b287a3da7bd3a0
Graham Howden <graham_howden1@yahoo.co.uk>
2020-04-17 17:43:38 +0100

    fix /api/doc not working correctly, fixes #120

--- src/main/scala/gg/uhc/hosts/endpoints/docs/DocsRoute.scala
+++ src/main/scala/gg/uhc/hosts/endpoints/docs/DocsRoute.scala
@@ -1,15 +1,17 @@
 package gg.uhc.hosts.endpoints.docs
 
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
 class DocsRoute {
   def apply(): Route =
-    concat(
-      pathEndOrSingleSlash {
-        getFromFile("apidocs/index.html")
-      },
-      getFromDirectory("apidocs")
-    )
-
+      concat(
+        pathEndOrSingleSlash {
+          redirectToTrailingSlashIfMissing(StatusCodes.Found) {
+            getFromFile("apidocs/index.html")
+          }
+        },
+        getFromDirectory("apidocs")
+      )
 }
