263638e06ae26f8eb68824e0fff2113a93ddb2b8
Eluinhost <graham_howden1@yahoo.co.uk>
2026-08-28 23:46:20 +0100

    redact moderation log modifiers unless user is admin or hosting advisor

--- frontend/src/sagas/permissionModerationLog.ts
+++ frontend/src/sagas/permissionModerationLog.ts
@@ -1,14 +1,16 @@
 import { PermissionsApi } from '../api';
 import { SagaIterator } from 'redux-saga';
-import { put, call, takeLatest } from 'redux-saga/effects';
+import { put, call, takeLatest, select } from 'redux-saga/effects';
 import { RefreshPermissionModerationLog } from '../actions';
 import { PermissionModerationLogEntry } from '../models/PermissionModerationLogEntry';
+import { getAccessToken } from '../state/Selectors';
 
 function* fetchPermissionModerationLogSaga(): SagaIterator {
   try {
     yield put(RefreshPermissionModerationLog.started());
 
-    const result: PermissionModerationLogEntry[] = yield call(PermissionsApi.fetchPermissionModerationLog);
+    const accessToken: string | null = yield select(getAccessToken);
+    const result: PermissionModerationLogEntry[] = yield call(PermissionsApi.fetchPermissionModerationLog, accessToken);
 
     yield put(RefreshPermissionModerationLog.success({ result }));
   } catch (error) {
