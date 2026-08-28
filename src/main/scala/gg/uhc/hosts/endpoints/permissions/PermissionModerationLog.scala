f0457e52a55ca7c15a98e04e9ca023a3d9821448
Eluinhost <graham_howden1@yahoo.co.uk>
2026-08-28 23:46:20 +0100

    redact moderation log modifiers unless user is admin or hosting advisor

--- src/main/scala/gg/uhc/hosts/endpoints/permissions/PermissionModerationLog.scala
+++ src/main/scala/gg/uhc/hosts/endpoints/permissions/PermissionModerationLog.scala
@@ -1,7 +1,7 @@
 package gg.uhc.hosts.endpoints.permissions
 
 import org.apache.pekko.http.scaladsl.server.Directives._
-import org.apache.pekko.http.scaladsl.server.Route
+import org.apache.pekko.http.scaladsl.server.{Directive1, Route}
 import gg.uhc.hosts.CustomJsonCodec
 import gg.uhc.hosts.database.Database
 import gg.uhc.hosts.endpoints.{CustomDirectives, EndpointRejectionHandler}
@@ -11,12 +11,23 @@ class PermissionModerationLog(directives: CustomDirectives, database: Database) 
   import CustomJsonCodec._
   import directives._
 
+  private val redactedModifier = "moderation team"
+
   def apply(): Route =
     parameters("before".as[Int].?, "count" ? 20) { (before, count) =>
       handleRejections(EndpointRejectionHandler()) {
         validate(count >= 1 && count <= 50, "Count must be between 1-50") {
-          requireSucessfulQuery(database.getPermissionModerationLog(before, count)) { log =>
-            complete(log.asJson)
+          optionalJwtAuthentication { maybeSession =>
+            val canSeeModifiers: Directive1[Boolean] = maybeSession match {
+              case Some(session) => checkHasAtLeastOnePermission(List("admin", "hosting advisor"), session.username)
+              case None          => provide(false)
+            }
+            canSeeModifiers { canSee =>
+              requireSucessfulQuery(database.getPermissionModerationLog(before, count)) { log =>
+                val visible = if (canSee) log else log.map(_.copy(modifier = redactedModifier))
+                complete(visible.asJson)
+              }
+            }
           }
         }
       }
