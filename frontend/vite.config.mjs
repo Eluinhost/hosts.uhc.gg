a04f71e0ae65414b8da6b380dd836b24d2d0951a
Eluinhost <graham_howden1@yahoo.co.uk>
2026-09-15 01:19:40 +0100

    remove frontend from backend build in favour of using Caddy to serve
    - added dev + prod caddy envs with common config
    - removed backend route that existed to only serve metadata + titles and replaced with Caddy templating
      - templating in index.html with a simple Vite plugin to strip out dev/prod based on dev client/production build
      - new endpoint for /matches/{id}/meta that serves the legacyTitle, caddy uses it for templating
    - moved frontend build out of backend docker image, now exists within the caddy alongside the old calendar and is served by Caddy
    - removed old /assets url that I don't think has been used since before the migration to CRA,
    actually caused an issue because CRA served assets at /static and vite at /assets and caused a conflict

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { caddyTemplateBlocks } from './vite/caddyTemplateBlocks.mjs';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [caddyTemplateBlocks(), react()],
   server: {
     proxy: {
       '/api': 'http://localhost:10000',
